a76a6e4d385659b347ce323aa71325a6506314c1
Christian21321 <164412626+Christian21321@users.noreply.github.com>
2025-03-27 17:02:29 -0300

    clases Main.java y NotasProcessor.java actualizadas

---
+++ java_funcional/src/NotasProcessor.java
@@ -0,0 +1,28 @@
+package java_funcional.src;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+public class NotasProcessor {
+
+    // Método para generar un arreglo de notas aleatorias
+    public static List<Double> generarNotas(int cantidad) {
+        Random rand = new Random();
+        return DoubleStream.generate(() -> 1.0 + rand.nextDouble() * (5.0 - 1.0))
+                .limit(cantidad)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    // Procesamiento Funcional
+    public static Map<String, Long> procesarNotas(List<Double> notas) {
+        return Map.of(
+                "mayoresCuatro", notas.stream().parallel().filter(n -> n > 4.0).count(),
+                "menoresTresSeis", notas.stream().parallel().filter(n -> n < 3.6).count(),
+                "rangoTresSeisCuatro", notas.stream().parallel().filter(n -> n >= 3.6 && n < 4.0).count()
+        );
+    }
+}
+
+
